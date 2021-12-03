f526c649b446b674dc37bca1337ebd1d581cfbe3
Farukh <49403261+Joombey@users.noreply.github.com>
2021-12-03 17:36:49 +0300

    Практическая работа 11 Скачки конные

---
+++ Practice11.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+import java.util.*;
+
+public class Practice11 {
+    public static void main(String[] args) {
+        Solution HorseRace = new Solution();
+        HorseRace.StartRace(6);
+    }
+}
+
+class Horse extends Thread{
+    private int number;
+    Horse(int number){
+        this.number = number;
+        this.start();
+    }
+    @Override
+    public void run() {
+        synchronized (this) {
+            System.out.println("Финишировал конь №" + number);
+        }
+    };
+}
+
+class Solution{
+    public void StartRace(int n){
+        Horse hr;
+        for(int i = 0; i < n; i++) {
+            hr = new Horse(i);
+        }
+    }
+}
